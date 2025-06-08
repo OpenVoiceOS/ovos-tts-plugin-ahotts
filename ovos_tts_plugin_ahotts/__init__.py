76318ed761fed78bc6d4d0ef74bea2bf08771101
JarbasAI <33701864+JarbasAl@users.noreply.github.com>
2025-06-08 03:56:10 +0100

    fix: compatibility with ovos-plugin-manager 1.X.X (#1)

--- ovos_tts_plugin_ahotts/__init__.py
+++ ovos_tts_plugin_ahotts/__init__.py
@@ -10,8 +10,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
 from ovos_plugin_manager.templates.tts import TTS
+from ovos_utils import classproperty
 from ovos_utils.log import LOG
 from pyahotts import AhoTTS
 
@@ -44,8 +44,8 @@ class AhoTTSPlugin(TTS):
 
         return (wav_file, None)  # No phonemes
 
-    @property
-    def available_languages(self) -> set:
+    @classproperty
+    def available_languages(cls) -> set:
         """Return languages supported by this TTS implementation in this state
         This property should be overridden by the derived class to advertise
         what languages that engine supports.
